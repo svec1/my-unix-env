017776817ad57a438e67d93ace607e46a065fa34
Charlie Root <root@svec..>
2025-11-13 14:38:37 +0300

    adds support openbsd

--- install.sh
+++ install.sh
@@ -2,20 +2,51 @@ sudo pacman -Syu zsh gcc make terminus-font
 
 git submodule update --init
 
+if [ "$1" == "openbsd" ]; then
+	echo "Was choose openbsd cfg."
+	cp openbsd_configs_make/dwm.mk dwm/config.mk
+	cp openbsd_configs_make/dmenu.mk dmenu/config.mk
+	cp openbsd_configs_make/slstatus.mk slstatus/config.mk
+	cp openbsd_configs_make/st.mk st/config.mk
+elif [ "$1" != "linux" ]; then
+	echo "Undefined cfg."
+	exit 1
+fi
+
+if [ "$2" == "full" ]; then
+	echo "Was choose full cfg."
+	echo "---BYEDPI BUILDING---"
+
+	cd byedpi
+	sudo make clean install
+	cd ..
+
+	cp display.sh ~
+	cp inet.sh ~
+fi
+
+echo "---DWM BUILDING---"
+
 cd dwm
 cp ../dwm-config.h config.h
 sudo make clean install
 cd ..
+
+echo "---DMENU BUILDING---"
 
 cd dmenu 
 cp ../dmenu-config.h config.h
 sudo make clean install
 cd ..
 
+echo "---SLSTATUS BUILDING---"
+
 cd slstatus 
 cp ../slstatus-config.h config.h
 sudo make clean install
 cd ..
+
+echo "---ST BUILDING---"
 
 cd st
 cp ../st-config.h config.h
@@ -26,10 +57,4 @@ git apply st-scrollback-mouse-0.9.2.diff
 sudo make clean install
 cd ..
 
-cd byedpi
-sudo make clean install
-cd ..
-
-cp display.sh ~
-cp inet.sh ~
-cp .xinirc ~
+cp .xinitrc ~
